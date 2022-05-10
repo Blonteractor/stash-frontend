a791590edeee70c646b5a86932e1df1513810261
Mayank Tomar <40265635+0Mayank@users.noreply.github.com>
2022-05-10 16:37:59 +0530

    did some shit mayhaps

---
+++ src/components/Homepage.vue
@@ -0,0 +1,28 @@
+<script>
+import Sidebar from "./Sidebar.vue";
+import Card from "./Card.vue";
+
+export default {
+  components: {
+    Card,
+  },
+};
+</script>
+
+<template>
+  <div id="bg">
+    <!-- <Sidebar /> -->
+    <Card />
+  </div>
+</template>
+
+<style scoped>
+#bg {
+  position: absolute;
+  height: 100%;
+  left: 300px;
+  width: 100%-300px;
+  top: 0px;
+  background-color: #113536;
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+<template>
+  <div></div>
+</template>
+
+<style scoped>
+div {
+  /* Side Rectangle */
+  position: fixed;
+  height: 100%;
+  width: 300px;
+  background: #112123;
+  left: 0;
+  top: 0;
+}
+</style>
