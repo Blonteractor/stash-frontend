603a65635e765c82f6763a958d231c0fdcd64283
KartikSindura <kartik.sindura@gmail.com>
2022-05-11 12:51:12 +0530

    app

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,39 @@
 import HelloWorld from "./components/HelloWorld.vue";
 import Homepage from "./components/Homepage.vue";
 import Sidebar from "./components/Sidebar.vue";
+import Cards from "./components/Cards.vue";
 </script>
 
 <template>
-<div className="homepage">
-  <Homepage />
-  <Sidebar />
-</div>
+  <div className="flex-container">
+    <div className="sidebar-container">
+      <Sidebar />
+    </div>
+    <div className="cards-container">
+      <Cards />
+    </div>
+  </div>
 </template>
 
+<style scoped>
+.flex-container {
+  display: flex;
+  background-color: #113536;
+  gap: 43px;
+  height: 100%;
+  width: 100%;
+}
+.sidebar-container {
+  flex: 1;
+  background-color: #112123;
+}
+.cards-container {
+  flex: 4;
+}
+</style>
+
 <style>
-
-
+body, html, #app {
+  margin: 0;
+}
 </style>
